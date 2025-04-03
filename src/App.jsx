37dcc081e27c71686214bb20de513382bc358e8d
jacobedwards-creator <71399496+jacobedwards-creator@users.noreply.github.com>
2025-04-03 11:48:35 -0500

    troubleshooting

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
-
+  console.log(puppies)
   return (
     <>
       <div>
+      { 
+   puppies.map((puppy) => {
+     return <p>{puppy.name}</p>
+   })
+}
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -31,5 +37,5 @@ function App() {
     </>
   )
 }
-
+const [featPupId, setFeatPupId] = useState(null)
 export default App
